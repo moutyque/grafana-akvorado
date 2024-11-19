bb83430261cb7ed5b9752f376a222092cb90470d
Quentin MOUTY <quentin.mouty@ovhcloud.com>
2024-11-19 17:24:25 +0100

    Generate LICENCES from github

--- generate-licenses.js
+++ generate-licenses.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const { execSync } = require('child_process');
+const {execSync} = require('child_process');
+const axios = require('axios');
 
 // Get the list of production dependencies in JSON format
 const listedPackagesJson = execSync('npm list -prod -depth 0 --json')
-  .toString();
+    .toString();
 
 // Parse the JSON to extract the package names and versions
 const listedPackages = Object.entries(JSON.parse(listedPackagesJson).dependencies)
-  .map(([key, value]) => `${key}@${value.version}`)
-  .join(';');
+    .map(([key, value]) => `${key}@${value.version}`)
+    .join(';');
 
 let cmd = `license-checker --json --packages '${listedPackages}' > licenses.json`;
 console.log(cmd);
 
 // Run license-checker on the listed packages and output to licenses.json
-execSync(cmd, { stdio: 'inherit' });
+execSync(cmd, {stdio: 'inherit'});
 
 // Read the licenses.json file
 const licensesFilePath = path.join(__dirname, 'licenses.json');
@@ -26,9 +27,14 @@ const licensesSet = new Set();
 const packages = [];
 
 for (const [packageName, licenseInfo] of Object.entries(licensesData)) {
-  const license = licenseInfo.licenses || 'Unknown';
-  licensesSet.add(license);
-  packages.push({ name: packageName, license, licenseFile: licenseInfo.licenseFile });
+    const license = licenseInfo.licenses || 'Unknown';
+    licensesSet.add(license);
+    packages.push({
+        name: packageName,
+        license,
+        licenseFile: licenseInfo.licenseFile,
+        repository: licenseInfo.repository
+    });
 }
 
 // Convert sets to arrays and sort them
@@ -37,48 +43,52 @@ const sortedPackages = packages.sort((a, b) => a.name.localeCompare(b.name));
 
 // Create directories and copy license files
 const outputDir = path.join(__dirname, 'LICENSES');
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir, { recursive: true });
+if (fs.existsSync(outputDir)) {
+    fs.rmSync(outputDir, {force: true, recursive: true});
 }
+fs.mkdirSync(outputDir, {recursive: true});
 
-sortedPackages.forEach(({ name, license, licenseFile }) => {
-  const packageDir = path.join(outputDir, name);
-  if (!fs.existsSync(packageDir)) {
-    fs.mkdirSync(packageDir, { recursive: true });
-  }
-  if (licenseFile) {
-    const licensePath = path.resolve(licenseFile);
 
-    if (fs.lstatSync(licensePath).isFile() && licensePath.endsWith('LICENSE')) {
-      // If licenseFile is a file, copy it directly
-      const destFilePath = path.join(packageDir, 'LICENSE');
-      fs.copyFileSync(licensePath, destFilePath);
-    } else  {
-      // If licenseFile is a directory, search for LICENSE files
-      const dir = path.dirname(licensePath)
-      const files = fs.readdirSync(dir);
-      const licenseFiles = files.filter(file => file.startsWith('LICENSE'));
-      if (licenseFiles.length > 0) {
-        licenseFiles.forEach(file => {
-          const sourceFilePath = path.join(dir, file);
-          const destFilePath = path.join(packageDir, 'LICENSE');
-          fs.copyFileSync(sourceFilePath, destFilePath);
-        });
-      } else {
-        // If no LICENSE file is found, write the license text to the file
-        fs.writeFileSync(path.join(packageDir, 'LICENSE'), license, 'utf8');
-      }
+sortedPackages.forEach(async ({name, license, licenseFile, repository}) => {
+    const packageDir = path.join(outputDir, name);
+    if (!fs.existsSync(packageDir)) {
+        fs.mkdirSync(packageDir, {recursive: true});
     }
-  } else {
-    // If no licenseFile is provided, write the license text to the file
-    fs.writeFileSync(path.join(packageDir, 'LICENSE'), license, 'utf8');
-  }
+
+    if (repository) {
+        const repoUrl = new URL(repository);
+        const [owner, repo] = repoUrl.pathname.split('/').filter(Boolean);
+        const apiUrl = `https://api.github.com/repos/${owner}/${repo}/license`;
+
+        try {
+            const response = await axios.get(apiUrl, {
+                headers: {
+                    'Accept': 'application/vnd.github+json',
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            });
+
+            const licenseContent = response.data.content;
+            // Add specific headers based on the license type
+            let finalContent = Buffer.from(licenseContent, 'base64').toString('utf8');
+            const destFilePath = path.join(packageDir, 'LICENSE');
+            finalContent = `${name}\n\n${finalContent}`
+            fs.writeFileSync(destFilePath, finalContent, 'utf8');
+        } catch (error) {
+            console.error(`Error fetching license for ${name}:`, error.message);
+            // If no LICENSE file is found, write the license text to the file
+            fs.writeFileSync(path.join(packageDir, 'LICENSE'), license, 'utf8');
+        }
+    } else {
+        // If no repository is provided, write the license text to the file
+        fs.writeFileSync(path.join(packageDir, 'LICENSE'), license, 'utf8');
+    }
 });
 
 // Generate the output string for the summary file
 const outputSummary = `Licences:\n${licenses.join('\n')}\n\nPackages:\n${sortedPackages
-  .map(({ name, license }) => `${name}; ${license}`)
-  .join('\n')}`;
+    .map(({name, license}) => `${name}; ${license}`)
+    .join('\n')}`;
 
 // Write the output summary to a file
 const outputSummaryFilePath = path.join(outputDir, 'LICENCES.txt');
